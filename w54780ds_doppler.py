3134c58153d59311ae339f02d15f08f9428344dd
dszablonski <dszab@protonmail.com>
2024-04-09 10:14:00 +0100

    initial commit

---
+++ w54780ds_doppler.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 09 2024
+_________Title____________
+
+"""
+import scipy as sp
+import numpy as np
+import matplotlib.pyplot as plt
+
+EMITTED_WAVELENGTH = 656.281E-9
+INITIAL_VELOCITY_GUESS = 50
+INITIAL_ANGULAR_VELOCITY_GUESS = 3e-8
+
+
+def main() -> None:
+    print(INITIAL_ANGULAR_VELOCITY_GUESS)
+
+
+if __name__ == '__main__':
+    main()
